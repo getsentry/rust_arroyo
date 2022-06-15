be9050b05f4c3af68030f67686fe076d9083ddd5
Vlad Kluev <vlad.kluev@sentry.io>
2022-06-15 15:02:39 -0700

    wip, nothing works, don't know how to collect futures

---
+++ src/bin/consumer/passthrough.rs
@@ -0,0 +1,169 @@
+use clap::{App, Arg};
+use futures::{stream::FuturesUnordered, StreamExt};
+use log::warn;
+use rdkafka::client::ClientContext;
+use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
+use rdkafka::consumer::stream_consumer::StreamConsumer;
+use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext, Rebalance};
+use rdkafka::error::KafkaResult;
+use rdkafka::message::Message;
+use rdkafka::producer::future_producer::OwnedDeliveryResult;
+use rdkafka::producer::DeliveryFuture;
+use rdkafka::producer::{FutureProducer, FutureRecord};
+use rdkafka::topic_partition_list::TopicPartitionList;
+use rdkafka::util::get_rdkafka_version;
+use std::time::Duration;
+
+// use crate::example_utils::setup_logger;
+
+// mod example_utils;
+//
+//
+
+// A context can be used to change the behavior of producers and consumers by adding callbacks
+// that will be executed by librdkafka.
+// This particular context sets up custom callbacks to log rebalancing events.
+struct CustomContext;
+
+impl ClientContext for CustomContext {}
+
+impl ConsumerContext for CustomContext {
+    fn pre_rebalance(&self, rebalance: &Rebalance) {
+        println!("Pre rebalance {:?}", rebalance);
+    }
+
+    fn post_rebalance(&self, rebalance: &Rebalance) {
+        println!("Post rebalance {:?}", rebalance);
+    }
+
+    fn commit_callback(&self, result: KafkaResult<()>, _offsets: &TopicPartitionList) {
+        println!("Committing offsets: {:?}", result);
+    }
+}
+
+// A type alias with your custom consumer can be created for convenience.
+type LoggingConsumer = StreamConsumer<CustomContext>;
+
+async fn wait_on_batch(batch: &mut FuturesUnordered<OwnedDeliveryResult>) {
+    // TODO: configurable batch size
+    if batch.len() > 10 {
+        while let Some(message) = batch.next().await {
+            println!("produced message: {}", message);
+        }
+    }
+}
+
+async fn consume_and_print(brokers: &str, group_id: &str, source_topic: &str, dest_topic: &str) {
+    let context = CustomContext {};
+    let batch = FuturesUnordered::new();
+    // TODO: make this not leak memory infinitely
+    let mut produce_batch: Vec<&str> = Vec::new();
+
+    let consumer: LoggingConsumer = ClientConfig::new()
+        .set("group.id", group_id)
+        .set("bootstrap.servers", brokers)
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        // TODO: disable auto commit
+        .set("enable.auto.commit", "true")
+        //.set("statistics.interval.ms", "30000")
+        .set("auto.offset.reset", "smallest")
+        // TODO: this should probably not be DEBUG
+        .set_log_level(RDKafkaLogLevel::Debug)
+        .create_with_context(context)
+        .expect("Consumer creation failed");
+
+    consumer
+        .subscribe(&vec![source_topic])
+        .expect("Can't subscribe to specified topics");
+
+    let producer: &FutureProducer = &ClientConfig::new()
+        .set("bootstrap.servers", brokers)
+        .set("message.timeout.ms", "5000")
+        .create()
+        .expect("couldn't create producer");
+
+    loop {
+        match consumer.recv().await {
+            Err(e) => warn!("Kafka error: {}", e),
+            Ok(m) => {
+                let payload = match m.payload_view::<str>() {
+                    None => "",
+                    Some(Ok(s)) => s,
+                    Some(Err(e)) => {
+                        warn!("Error while deserializing message payload: {:?}", e);
+                        ""
+                    }
+                };
+                println!("key: '{:?}', payload: '{}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
+                      m.key(), payload, m.topic(), m.partition(), m.offset(), m.timestamp());
+                let to_send = format!("{}", payload);
+                produce_batch.push(to_send.as_str());
+                let produce_future = producer.send(
+                    FutureRecord::to(dest_topic)
+                        .payload(&produce_batch.last().unwrap())
+                        .key("None"),
+                    Duration::from_secs(0),
+                );
+
+                batch.push(produce_future);
+
+                consumer.commit_message(&m, CommitMode::Async).unwrap();
+            }
+        };
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let matches = App::new("consumer example")
+        .version(option_env!("CARGO_PKG_VERSION").unwrap_or(""))
+        .about("Simple command line consumer")
+        .arg(
+            Arg::with_name("brokers")
+                .short("b")
+                .long("brokers")
+                .help("Broker list in kafka format")
+                .takes_value(true)
+                .default_value("localhost:9092"),
+        )
+        .arg(
+            Arg::with_name("group-id")
+                .short("g")
+                .long("group-id")
+                .help("Consumer group id")
+                .takes_value(true)
+                .default_value("example_consumer_group_id"),
+        )
+        .arg(
+            Arg::with_name("log-conf")
+                .long("log-conf")
+                .help("Configure the logging format (example: 'rdkafka=trace')")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("source_topic")
+                .long("source")
+                .help("source topic name")
+                .default_value("test_source")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("dest_topic")
+                .long("dest")
+                .help("destination topic name")
+                .default_value("test_dest")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let (version_n, version_s) = get_rdkafka_version();
+    println!("rd_kafka_version: 0x{:08x}, {}", version_n, version_s);
+
+    let source_topic = matches.value_of("source_topic").unwrap();
+    let brokers = matches.value_of("brokers").unwrap();
+    let group_id = matches.value_of("group-id").unwrap();
+    let dest_topic = matches.value_of("dest_topic").unwrap();
+
+    consume_and_print(brokers, group_id, source_topic, dest_topic).await
+}
